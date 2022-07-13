60960b260c98a004bfd58a2d375d190bce566cea
ramyashilpas <ramyashilpas@gmail.com>
2022-07-13 18:33:03 -0400

    leet code problems

--- src/main/java/shilpa/leetcode/TwoSumEasyLevel.java
+++ src/main/java/shilpa/leetcode/TwoSumEasyLevel.java
@@ -1,24 +1,25 @@
 package shilpa.leetcode;
 
 public class TwoSumEasyLevel {
-        public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
 
-            for (int i=0;i<=nums.length;i++){
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[i]+nums[j]==target){
-                        return new int[]{i,j};
-                    }
-
+        for (int i = 0; i <= nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
-            }
-            return null;
-        }
-        public static void main(String[] args){
-            TwoSumEasyLevel s=new TwoSumEasyLevel();
-            int[] b=new int[] {2,7,11,15};
-            s.twoSum(b,9);
-            System.out.println("indices of two numbers adding up to target are:"+s);
 
+            }
         }
+        return null;
     }
+
+    public static void main(String[] args) {
+        TwoSumEasyLevel s = new TwoSumEasyLevel();
+        int[] b = new int[]{2, 7, 11, 15};
+        s.twoSum(b, 9);
+        System.out.println("indices of two numbers adding up to target are:" + s);
+
+    }
+}
 
