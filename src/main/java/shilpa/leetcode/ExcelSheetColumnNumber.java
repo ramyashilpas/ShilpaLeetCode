3685acc2e8b7355728dbb5da0aa66069b9ce1763
ramyashilpas <ramyashilpas@gmail.com>
2022-09-01 17:59:17 -0400

    Leetcode Easylevel 1 problems solved.

--- src/main/java/shilpa/leetcode/ExcelSheetColumnNumber.java
+++ src/main/java/shilpa/leetcode/ExcelSheetColumnNumber.java
@@ -1,13 +1,21 @@
 package shilpa.leetcode;
 
+
 public class ExcelSheetColumnNumber {
     public static int titleToNumber(String columnTitle) {
-        int i=Integer.parseInt(columnTitle);
-      return i;
+        if(columnTitle.length()==0){
+            return 0;
+        }
+        int sum=0;
+      for(int i=0;i<columnTitle.length();i++){
+          sum*=26;
+          sum+= (columnTitle.charAt(i)-64);
+      }
+      return sum;
     }
 
     public static void main(String[] args) {
-        String columnTitle="A";
+        String columnTitle="ZY";
         int i = ExcelSheetColumnNumber.titleToNumber(columnTitle);
         System.out.println(i);
     }
