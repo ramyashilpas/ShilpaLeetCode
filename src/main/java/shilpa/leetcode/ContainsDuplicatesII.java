acfd09bef0d454a693265d4c6e410cda873e57d1
ramyashilpas <ramyashilpas@gmail.com>
2022-09-15 18:17:32 -0400

    Leetcode Easylevel 1 problem solved.

--- src/main/java/shilpa/leetcode/ContainsDuplicatesII.java
+++ src/main/java/shilpa/leetcode/ContainsDuplicatesII.java
@@ -7,14 +7,14 @@ import java.util.HashMap;
 public class ContainsDuplicatesII {
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer,Integer> hs=new HashMap<>();
-        for(int i:nums){
-            hs.put(nums[i],i);
+        for(int i=0; i<nums.length; i++){
             if (hs.containsKey(nums[i])) {
                 int result = i - hs.get(nums[i]);
                 if (result <= k) {
                     return true;
                 }
             }
+            hs.put(nums[i],i);
         }
         return false;
     }
